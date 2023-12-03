194b50bf5c90fa800c40949523ec943e798e8cfc
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2023-12-03 22:12:13 +0200

    added necessary componants

--- src/App.js
+++ src/App.js
@@ -1,53 +1,29 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HireMe from "./components/HireMe";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import Service from "./components/Service";
+import Skills from "./components/Skills";
+import Testimonial from "./components/Testimonial";
+
+
 function App() {
   return (
-    <body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
-
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" data-spy="affix" data-offset-top="0">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            
-        </div>          
-    </nav>
-    <header class="header" id="home">
-        <div class="container">
-            <div class="infos">
-                <h6 class="subtitle">hello,I'm</h6>
-                <h6 class="title">James Smith</h6>
-                <p>UI/UX Designer</p>
-
-                <div class="buttons pt-3">
-                    <button class="btn btn-primary rounded">HIRE ME</button>
-                    <button class="btn btn-dark rounded">DOWNLOAD CV</button>
-                </div>      
-
-                <div class="socials mt-4">
-                </div>
-            </div>              
-            <div class="img-holder">
-                <img src="assets/imgs/man.svg" alt=""/>
-            </div>      
-        </div>  
-
-        <div class="widget">
-            <div class="widget-item">
-                <h2>124</h2>
-                <p>Happy Clients</p>
-            </div>
-            <div class="widget-item">
-                <h2>456</h2>
-                <p>Project Completed</p>
-            </div>
-            <div class="widget-item">
-                <h2>789</h2>
-                <p>Awards Won</p>
-            </div>
-        </div>
-    </header>
-    
-</body>
+    <>
+        <NavBar/>
+        <Header/>
+        <About/>
+        <Service/>
+        <Skills/>
+        <Portfolio/>
+        <Testimonial/>
+        <HireMe/>
+        <Contact/>
+        <Footer/>
+    </>
   );
 }
 
