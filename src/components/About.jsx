c98cb17e74abd413c73fb888ea911b3a5c0513ab
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2024-01-11 15:05:11 +0200

    updated cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,31 @@
-export default function About(){
-    return (
-        <section id="about" class="section mt-3">
-            <div class="container mt-5">
-                <div class="row text-center text-md-left">
-                    <div class="col-md-3">
-                        <img src="assets/imgs/avatar-madushan.png" alt="" class="img-thumbnail mb-4"/>
-                    </div>
-                    <div class="pl-md-4 col-md-9">
-                        <h6 class="title">Vimukthi Madushan</h6>
-                        <p class="subtitle">Full Stack Developer</p>
-                        <p>Hello! I'm a Full Stack Developer with 4+ years of experience in creating efficient web solutions. I specialize in Node.js for server-side development, use React for crafting user-friendly interfaces, and excel in Laravel OctoberCMS for building strong backends that drive web applications.</p>
-                        <a href="assets/cv/madushan-cv.pdf" target="__blank"><button class="btn btn-primary rounded mt-3">DOWNLOAD CV</button></a>                   
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+export default function About() {
+  return (
+    <section id="about" class="section mt-3">
+      <div class="container mt-5">
+        <div class="row text-center text-md-left">
+          <div class="col-md-3">
+            <img
+              src="assets/imgs/avatar-madushan.png"
+              alt=""
+              class="img-thumbnail mb-4"
+            />
+          </div>
+          <div class="pl-md-4 col-md-9">
+            <h6 class="title">Vimukthi Madushan</h6>
+            <p class="subtitle">Full Stack Developer</p>
+            <p>
+              Hello! I'm a Full Stack Developer with 4+ years of experience in
+              creating efficient web solutions. I specialize in Node.js for
+              server-side development, use React for crafting user-friendly
+              interfaces, and excel in Laravel OctoberCMS for building strong
+              backends that drive web applications.
+            </p>
+            <a href="assets/cv/CV2-madushan.pdf" target="__blank">
+              <button class="btn btn-primary rounded mt-3">DOWNLOAD CV</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
